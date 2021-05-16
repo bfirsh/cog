59aa7073bd00045c1d804faf16b994ca9c710cae
Ben Firshman <ben@firshman.co.uk>
2021-05-15 17:16:03 -0700

    Make non-interactive on TERM=dumb
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- pkg/util/terminal/basic.go
+++ pkg/util/terminal/basic.go
@@ -3,6 +3,7 @@ package terminal
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/containerd/console"
 	"github.com/mattn/go-isatty"
@@ -12,20 +13,22 @@ import (
 // Returns a UI which will write to the current processes
 // stdout/stderr.
 func ConsoleUI(ctx context.Context) UI {
-	// We do both of these checks because some sneaky environments fool
+	// We do all of these checks because some sneaky environments fool
 	// one or the other and we really only want the glint-based UI in
 	// truly interactive environments.
-	glint := isatty.IsTerminal(os.Stdout.Fd()) && sshterm.IsTerminal(int(os.Stdout.Fd()))
-	if glint {
-		glint = false
+	interactive := (isatty.IsTerminal(os.Stdout.Fd()) &&
+		sshterm.IsTerminal(int(os.Stdout.Fd())) &&
+		strings.ToLower(os.Getenv("TERM")) != "dumb")
+	if interactive {
+		interactive = false
 		if c, err := console.ConsoleFromFile(os.Stdout); err == nil {
 			if sz, err := c.Size(); err == nil {
-				glint = sz.Height > 0 && sz.Width > 0
+				interactive = sz.Height > 0 && sz.Width > 0
 			}
 		}
 	}
 
-	if glint {
+	if interactive {
 		return GlintUI(ctx)
 	} else {
 		return NonInteractiveUI(ctx)
